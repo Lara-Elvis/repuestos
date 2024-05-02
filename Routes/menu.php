1584d29ede6ba6a108d4ffcf2ea9177307a79a68
Elvis Lara <laraelvisinar@gmail.com>
2024-05-02 13:13:13 -0600

    Crea el menu.php

---
+++ Routes/menu.php
@@ -0,0 +1,13 @@
+<header>
+        <h1>Repuestos Escalante</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="#acerca">Acerca de la Empresa</a></li>
+            <li><a href="#crear-usuario">Crear Usuario y Contraseña</a></li>
+            <li><a href="#mantenimientos">Mantenimientos</a></li>
+            <li><a href="#reportes">Reportes</a></li>
+            <li><a href="#procesos">Procesos o Movimientos</a></li>
+        </ul>
+    </nav>
--- index.php
+++ index.php
@@ -4,19 +4,7 @@
     <?php include("Routes/head.php")?>
 </head>
 <body>
-    <header>
-        <h1>Repuestos Escalante</h1>
-    </header>
-
-    <nav>
-        <ul>
-            <li><a href="#acerca">Acerca de la Empresa</a></li>
-            <li><a href="#crear-usuario">Crear Usuario y Contraseña</a></li>
-            <li><a href="#mantenimientos">Mantenimientos</a></li>
-            <li><a href="#reportes">Reportes</a></li>
-            <li><a href="#procesos">Procesos o Movimientos</a></li>
-        </ul>
-    </nav>
+    <?php include("Routes/menu.php")?>
 
     <section id="acerca">
         <h2>Acerca de la Empresa</h2>
