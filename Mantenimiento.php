3619ef9f886401674fa15513e2b34b3037590752
Elvis Lara <laraelvisinar@gmail.com>
2024-05-02 13:28:08 -0600

    agrega el Mantenimiento.ph y modifica el menu

---
+++ Mantenimiento.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php include("Routes/head.php")?>
+    <title>Mantenimiento</title>
+</head>
+<body>
+    <?php include("Routes/menu.php")?>
+    <section id="mantenimientos">
+        <h2>Mantenimientos</h2>
+        <ul>
+            <li><a href="#clientes">Clientes</a></li>
+            <li><a href="#productos">Productos</a></li>
+        </ul>
+    </section>
+</body>
+</html>
